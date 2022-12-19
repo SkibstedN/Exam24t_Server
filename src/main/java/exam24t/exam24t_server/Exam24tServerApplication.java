fb95b4305f06f01389cc4925c395d45871344bc6
SkibstedN <niko933n@stud.kea.dk>
2022-12-19 09:02:06 +0100

    Initial Commit eksamen

---
+++ src/main/java/exam24t/exam24t_server/Exam24tServerApplication.java
@@ -0,0 +1,13 @@
+package exam24t.exam24t_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exam24tServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Exam24tServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/exam24t/exam24t_server/Exam24tServerApplicationTests.java
@@ -0,0 +1,13 @@
+package exam24t.exam24t_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exam24tServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
